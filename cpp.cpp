23f57b3badffe8ede3c7917ca88d6eae5150eef8
nemusou <ryubi.102395@gmail.com>
2016-08-28 12:25:56 +0900

    add in() and print()

--- cpp.cpp
+++ cpp.cpp
@@ -7,23 +7,38 @@ typedef queue<int> qi;
 typedef stack<int> si;
 typedef deque<int> dqi;
 
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define REP(i, n) for (int i = 0; i < n; i++)
 #define FOR(i, a, b) for (int i = a; i < b; i++)
-#define pf push_front
-#define pb push_back
+#define PF push_front
+#define PB push_back
 #define FST first
 #define SEC second
 #define sz(obj) ((int)(obj).size())
-
-inline int gcd(int a, int b){if (a == 0) return (b); gcd(b % a, b);}
+#define ALL(v) (v).begin(), (v).end()
 
 const int INF = 999999999;
 const int MOD = 1e9 + 7;
 const int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
+
+inline int in() {int x; std::cin >> x; return x;}
+ 
+template <typename T>
+void print(std::vector<T>& v, char c = ' ') {
+  REP(i, v.size()) {
+    if (i != 0) std::cout << c;
+    std::cout << v[i];
+  }
+  std::cout << endl;
+}
+template <typename T> void print(T x) { std::cout << x << '\n'; }
+
+int gcd(int a, int b){ int c; while (a != 0){ c = a; a = b % a; b = c; } return b; }
 
 signed main()
 {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
+
+  return (0);
 }
