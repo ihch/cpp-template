cf5f0b6a5dd2450aee0d8179202af06aed2da375
nemu_sou <ryubi.102395@gmail.com>
2016-09-23 09:53:35 +0900

    add macro + template

--- cpp.cpp
+++ cpp.cpp
@@ -1,27 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef queue<int> qi;
 typedef stack<int> si;
 typedef deque<int> dqi;
 
-#define REP(i, n) for (int i = 0; i < n; i++)
-#define FOR(i, a, b) for (int i = a; i < b; i++)
-#define PF push_front
-#define PB push_back
-#define FST first
-#define SEC second
+#define reps(i, a, b) for (int i = a; i < b; ++i)
+#define rep(i, n) reps(i, 0, n)
+#define REPS(i, a, b) reps(i, a, b)
+#define REP(i, n) rep(i, n)
+#define deps(i, a, b) for (int i = a; i >= b; --i)
+#define dep(i, n) deps(i, n, 0)
+#define DEPS(i, a, b) deps(i, a, b)
+#define DEP(i, n) dep(i, n)
+
+#define pf push_front
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define FST fst
+#define sec second
+#define SEC sec
 #define sz(obj) ((int)(obj).size())
-#define ALL(v) (v).begin(), (v).end()
+#define all(v) (v).begin(), (v).end()
+#define ALL(v) all(v)
 
 const int INF = 999999999;
 const int MOD = 1e9 + 7;
 const int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
 
+inline bool inside(int y, int x, int H, int W) {return y >= 0 && x >= 0 && y < H && x < W;}
 inline int in() {int x; std::cin >> x; return x;}
- 
 template <typename T>
 void print(std::vector<T>& v, char c = ' ') {
   REP(i, v.size()) {
@@ -32,13 +46,8 @@ void print(std::vector<T>& v, char c = ' ') {
 }
 template <typename T> void print(T x) { std::cout << x << '\n'; }
 
-int gcd(int a, int b){ int c; while (a != 0){ c = a; a = b % a; b = c; } return b; }
-
 signed main()
 {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-
 
   return (0);
 }
